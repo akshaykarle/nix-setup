8bdd3f70aa7fb30d2ddf93e2d75ccfdcf484b120
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2022-12-30 12:33:28 +0000

    link repo into /home/akshaykarle/.config/nixpkgs

--- install-nix.sh
+++ install-nix.sh
@@ -7,11 +7,15 @@ else
     source ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
+# enable running flake
 NIX_CONF_PATH="$HOME/.config/nix"
 mkdir -p "$NIX_CONF_PATH"
 if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" <"$NIX_CONF_PATH/nix.conf"; then
     echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH"/nix.conf
 fi
+
+# link current dir into nixpkgs
+ln -sf $(pwd) $HOME/.config/nixpkgs
 
 # install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
@@ -19,5 +23,5 @@ nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
 
-# build and apply config from flake
+# build and activate config from flake
 home-manager switch --flake '.#akshaykarle'
