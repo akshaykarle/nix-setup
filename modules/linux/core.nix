cbde7e7baf1e268ed95e964c4bb56b478e48d7fa
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-12-07 15:15:25 +0000

    add linux packages back

--- modules/linux/core.nix
+++ modules/linux/core.nix
@@ -16,18 +16,18 @@
   };
 
   # environment setup
-  # environment = {
-  #   systemPackages = with pkgs; [
-  #     glibcLocales
-  #     coreutils-full
-  #     virtualbox
+  environment = {
+    systemPackages = with pkgs; [
+      glibcLocales
+      coreutils-full
+      virtualbox
 
-  #     # gui apps
-  #     signald
-  #     brave
-  #     libreoffice-fresh
-  #     spotify
-  #     vlc
-  #   ];
-  # };
+      # gui apps
+      signald
+      brave
+      libreoffice-fresh
+      spotify
+      vlc
+    ];
+  };
 }
