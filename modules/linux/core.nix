ace57656afa5c950c665f56deb8c8f00eb9c3359
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-12-19 16:22:58 +0000

    remove uneeded application linking from nix as we are nixos now

--- modules/linux/core.nix
+++ modules/linux/core.nix
@@ -1,21 +1,4 @@
 { self, inputs, config, pkgs, ... }: {
-  # See
-  # https://discourse.nixos.org/t/home-manager-installed-apps-dont-show-up-in-applications-launcher/8523/2
-  # https://github.com/nix-community/home-manager/issues/1439
-  targets.genericLinux.enable = true;
-
-  # temp fix as nix installer puts symlinks outside of XDG_DATA_DIRS and desktop files need to be executable in /nix/store
-  # References: https://old.reddit.com/r/Ubuntu/comments/hh1eh9/ubuntu_refuses_to_run_thirdparty_desktop_files/fw7yv0e/
-  # Copied from: https://github.com/nix-community/home-manager/issues/1439#issuecomment-1106208294
-  home.activation = {
-    linkDesktopApplications = {
-      after = [ "writeBoundary" "createXdgUserDirectories" ];
-      before = [ ];
-      data = "/usr/bin/update-desktop-database";
-    };
-  };
-
-  # environment setup
   environment = {
     systemPackages = with pkgs; [
       glibcLocales
