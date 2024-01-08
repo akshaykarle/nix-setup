96d9b3998a3e895c46c5b69c5108589c94c7afe5
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-01-08 10:47:55 +0000

    configure nvidia on nixos

--- modules/hardware/asus.nix
+++ modules/hardware/asus.nix
@@ -27,12 +27,42 @@
   hardware.cpu.amd.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  # configure nvidia driver based on: https://nixos.wiki/wiki/Nvidia
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     # PCI-Express Runtime D3 Power Management is enabled by default on this laptop
     # But it can fix screen tearing & suspend/resume screen corruption in sync mode
     modesetting.enable = lib.mkDefault true;
     # Enable DRM kernel mode setting
     powerManagement.enable = lib.mkDefault true;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     prime = {
       amdgpuBusId = "PCI:8:0:0";
