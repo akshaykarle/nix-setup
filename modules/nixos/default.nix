b30558a2737aa72b6299635874654da2dab968f2
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-08-13 13:49:59 +0100

    Revert "remove overloaded sound option from nixos"
    This reverts commit 55ada566214e7cc04e9aa23568691f35d36b4986.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -48,6 +48,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
