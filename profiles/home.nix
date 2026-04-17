5f81605324b6977aef364f2222952d3750506ce1
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2026-04-17 15:41:31 +0100

    nixfmt

--- profiles/home.nix
+++ profiles/home.nix
@@ -7,7 +7,10 @@
   ];
 
   # Allow SSH on local network interfaces (check with 'ip addr' and remove unused)
-  services.tailscale-ssh.lanInterfaces = [ "enp1s0" "enp0s20f0u2" ];
+  services.tailscale-ssh.lanInterfaces = [
+    "enp1s0"
+    "enp0s20f0u2"
+  ];
 
   user.name = "daksh-home";
   user.description = "Daksh Home";
