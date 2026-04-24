839a184fc471add086214dbed4e403c11f62b39d
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2026-04-24 14:56:29 +0100

    fix programs.ssh warning and start using matchBlocks

--- modules/home-manager/1password.nix
+++ modules/home-manager/1password.nix
@@ -4,13 +4,15 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      IdentityAgent "${
-        if pkgs.stdenv.isDarwin then
-          "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-        else
-          "~/.1password/agent.sock"
-      }"
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      extraOptions = {
+        "IdentityAgent" =
+          if pkgs.stdenv.isDarwin then
+            ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''
+          else
+            "~/.1password/agent.sock";
+      };
+    };
   };
 }
