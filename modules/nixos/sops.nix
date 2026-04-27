d94859d1681e58cd1e95c07d3e54dab658ca67e0
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2026-04-27 15:52:05 +0100

    add intel age key, fix sops key path and add new hashedpassword secrets

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -2,7 +2,7 @@
 {
   sops = {
     defaultSopsFile = ../../secrets/passwords.yaml;
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.sshKeyPaths = [ "/home/${config.user.name}/.ssh/id_ed25519" ];
 
     secrets."user-password-${config.user.name}" = {
       neededForUsers = true;
