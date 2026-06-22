5168e6c62575c3421f1a631a0671ab41c81c2f12
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2026-06-22 15:37:12 +0100

    fix setup.sh to add nix-command and flakes to nix cli directly
    * this is to fix new setup on darwin where we now have to run nix as root which doesn't inherit the nix.conf

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if command -v nix >/dev/null; then
-    echo "nix is already installed on this system."
+  echo "nix is already installed on this system."
 else
-    curl -L https://nixos.org/nix/install | sh
-    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  curl -L https://nixos.org/nix/install | sh
+  source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
 # enable running flake
 NIX_CONF_PATH="$HOME/.config/nix"
 mkdir -p "$NIX_CONF_PATH"
 if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" <"$NIX_CONF_PATH/nix.conf"; then
-    echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH"/nix.conf
+  echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH"/nix.conf
 fi
 
 # link current dir into nixpkgs
@@ -23,17 +23,14 @@ nix-channel --update
 
 NIX_CMD="${1:-switch}"
 
-if [ -n "$(uname -a | grep 'Darwin' | grep 'x86_64')" ]
-then
-    which brew > /dev/null || bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    sudo nix run nix-darwin -- $NIX_CMD --flake  '.#akshaykarle@x86_64-darwin'
-elif [ -n "$(uname -a | grep 'Darwin' | grep 'arm64')" ]
-then
-    which brew > /dev/null || bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    sudo nix run nix-darwin -- $NIX_CMD --flake  '.#akshaykarle@aarch64-darwin'
-elif [ -n "$(whoami | grep 'daksh-home')" ]
-then
-    sudo nixos-rebuild $NIX_CMD --flake .#"daksh-home@x86_64-linux"
+if [ -n "$(uname -a | grep 'Darwin' | grep 'x86_64')" ]; then
+  which brew >/dev/null || bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  sudo nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- $NIX_CMD --flake '.#akshaykarle@x86_64-darwin'
+elif [ -n "$(uname -a | grep 'Darwin' | grep 'arm64')" ]; then
+  which brew >/dev/null || bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  sudo nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- $NIX_CMD --flake '.#akshaykarle@aarch64-darwin'
+elif [ -n "$(whoami | grep 'daksh-home')" ]; then
+  sudo nixos-rebuild $NIX_CMD --flake .#"daksh-home@x86_64-linux"
 else
-    sudo nixos-rebuild $NIX_CMD --flake .#"akshaykarle@x86_64-linux"
+  sudo nixos-rebuild $NIX_CMD --flake .#"akshaykarle@x86_64-linux"
 fi
