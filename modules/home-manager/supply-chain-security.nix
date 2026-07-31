3fea7b97bdb0229da7a80581550c5ea720f799b5
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2026-07-31 10:52:16 +0100

    get rid of old npm config and email field

--- modules/home-manager/supply-chain-security.nix
+++ modules/home-manager/supply-chain-security.nix
@@ -13,30 +13,9 @@
         audit=true
         package-lock=true
         registry=https://registry.npmjs.org/
-        email=npm requires email to be set but doesn't use the value
 
         # Writable global install location (nix store is read-only)
         prefix=''${HOME}/.npm-global
-      ''
-      + pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-
-        # Azure DevOps Platforms registry
-        //pkgs.dev.azure.com/AxiCore/Platforms/_packaging/Platforms/npm/registry/:username=''${AZURE_NPM_USERNAME}
-        //pkgs.dev.azure.com/AxiCore/Platforms/_packaging/Platforms/npm/registry/:_password=''${AZURE_NPM_PASSWORD}
-        //pkgs.dev.azure.com/AxiCore/Platforms/_packaging/Platforms/npm/:username=''${AZURE_NPM_USERNAME}
-        //pkgs.dev.azure.com/AxiCore/Platforms/_packaging/Platforms/npm/:_password=''${AZURE_NPM_PASSWORD}
-        //pkgs.dev.azure.com/AxiCore/Platforms/_packaging/Platforms/npm/:email=npm requires email to be set but doesn't use the value
-
-        # Azure DevOps Portals registry
-        //pkgs.dev.azure.com/AxiCore/Portals/_packaging/npm_axi_portals/npm/registry/:username=''${AZURE_NPM_USERNAME}
-        //pkgs.dev.azure.com/AxiCore/Portals/_packaging/npm_axi_portals/npm/registry/:_password=''${AZURE_NPM_PASSWORD}
-        //pkgs.dev.azure.com/AxiCore/Portals/_packaging/npm_axi_portals/npm/:username=''${AZURE_NPM_USERNAME}
-        //pkgs.dev.azure.com/AxiCore/Portals/_packaging/npm_axi_portals/npm/:_password=''${AZURE_NPM_PASSWORD}
-        //pkgs.dev.azure.com/AxiCore/Portals/_packaging/npm_axi_portals/npm/:email=npm requires email to be set but doesn't use the value
-
-        # Accertify registry
-        //repository.device.accertify.com/artifactory/api/npm/inmobile-npm/:email=''${ACCERTIFY_NPM_USERNAME}
-        //repository.device.accertify.com/artifactory/api/npm/inmobile-npm/:_authToken=''${ACCERTIFY_NPM_TOKEN}
       '';
     };
     uv-config = {
